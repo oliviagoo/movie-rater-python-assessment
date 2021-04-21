2a102f0f19263491b55c22ae711a08bc5906a165
Olivia Goodman <oliviagoodman@Olivias-Air-3.hub>
2021-04-21 14:22:49 +1200

    versions 3 and 4
    (These versions are merged because version 4 was only a very small change).
    In these versions I added functionality to rate multiple movies. To do this I added next and back buttons, which allow the user to cycle through the movies.

--- movie program rating frame.py
+++ movie program rating frame.py
@@ -1,4 +1,4 @@
-#version 2 - fixing the radio buttons and the layout
+#version 4 - adding the back button functionality
 from tkinter import *
 
 RATINGS = ["No Rating", "1", "2", "3", "4", "5"]
@@ -12,6 +12,9 @@ class MovieRaterGUI:
     def __init__(self, parent):
         self.movies_list = []
         self.movies_list.append(Movie("The Hobbit"))
+        self.movies_list.append(Movie("The Titanic"))
+        self.movies_list.append(Movie("Jurassic Park"))
+        self.movies_list.append(Movie("Spirited Away"))
         self.position = 0
         self.rating = StringVar()
         self.rating.set(self.movies_list[self.position].rating)
@@ -36,10 +39,42 @@ class MovieRaterGUI:
             self.rating_list.append(rb)
 
         rb_frame.grid(row = 1, column = 1, padx = 10, pady = 10)
+
+        self.back_but = Button(self.rate_frame, text = "Previous", command = self.go_back, state = DISABLED)
+        self.back_but.grid(row = 2, column = 0)
+
+        self.next_but = Button(self.rate_frame, text = "Next", command = self.go_next)
+        self.next_but.grid(row = 2, column = 1)
+        
         self.rate_frame.grid(row = 0, column = 0)
 
     def printrate(self):
         print(self.rating.get())
+        self.movies_list[self.position].rating = self.rating.get()
+        print(self.movies_list[self.position].rating)
+
+    def go_next(self):
+        self.position += 1
+        self.movie_label.configure(text = self.movies_list[self.position].name)
+        self.check_pos()
+        self.rating.set(self.movies_list[self.position].rating)
+
+    def go_back(self):
+        self.position -= 1
+        self.movie_label.configure(text = self.movies_list[self.position].name)
+        self.check_pos()
+        self.rating.set(self.movies_list[self.position].rating)
+        
+    def check_pos(self):
+        if self.position == len(self.movies_list) - 1:
+            self.next_but.configure(state = DISABLED)
+        else:
+            self.next_but.configure(state = NORMAL)
+            
+        if self.position == 0:
+            self.back_but.configure(state = DISABLED)
+        else:
+            self.back_but.configure(state = NORMAL)
 
 #main routine
 if __name__ == "__main__":
