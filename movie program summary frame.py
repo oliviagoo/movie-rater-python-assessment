e227f84540ce119a9f14a2836935db34c4bbc78e
Olivia Goodman <oliviagoodman@Olivias-Air-3.hub>
2021-04-29 12:04:37 +1200

    version 7
    In this version, I set up the GUI for the summary frame. For now, this is just to sort out the layout of the frame so it isn't very functional.

---
+++ movie program summary frame.py
@@ -0,0 +1,29 @@
+#version 7 - setting up the summary frame GUI
+from tkinter import *
+from tkinter.scrolledtext import *
+
+RATINGS = ["No Rating", "1", "2", "3", "4", "5"]
+
+class MovieRaterGUI:
+    def __init__(self, parent):
+        self.search_rate = StringVar()
+        
+        self.summ_frame = Frame(parent)
+
+        info_label = Label(self.summ_frame, text = "You have given the following movies a rating of {}:".format(self.search_rate.get()))
+        info_label.grid(row = 0, column = 0)
+
+        self.movie_display = ScrolledText(self.summ_frame, width = 50, height = 10, wrap = "word")
+        self.movie_display.grid(row = 1, column = 0, padx = 10)
+
+        self.movie_display.insert(END, "Example text \nExample text etc. etc. etc. \n\n\n\n\nexample\n\nmore example text\n\n\n")
+        back_rate_but = Button(self.summ_frame, text = "Back to rating")
+
+        self.summ_frame.grid(row = 0, column = 0)
+
+#main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Movie Rating Program")
+    MovieRater = MovieRaterGUI(root)
+    root.mainloop()
